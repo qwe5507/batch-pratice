2afd8760255d3e64e6d40f1b44229ffa39bcf467
qwe5507 <apvmffkdls@gmail.com>
2025-07-04 18:45:02 +0900

    add jobParameters Sample

---
+++ src/main/java/com/system/batch/killbatchsystem/config/SystemInfiltrationParameters.java
@@ -0,0 +1,35 @@
+package com.system.batch.killbatchsystem.config;
+
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@StepScope
+@Component
+public class SystemInfiltrationParameters {
+    @Value("#{jobParameters[missionName]}")
+    private String missionName;
+    private int securityLevel;
+    private final String operationCommander;
+
+    public SystemInfiltrationParameters(@Value("#{jobParameters[operationCommander]}") String operationCommander) {
+        this.operationCommander = operationCommander;
+    }
+
+    @Value("#{jobParameters[securityLevel]}")
+    public void setSecurityLevel(int securityLevel) {
+        this.securityLevel = securityLevel;
+    }
+
+    public String getMissionName() {
+        return missionName;
+    }
+
+    public int getSecurityLevel() {
+        return securityLevel;
+    }
+
+    public String getOperationCommander() {
+        return operationCommander;
+    }
+}
