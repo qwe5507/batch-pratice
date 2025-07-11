630fba9cddf43d3c57faa3b19bba48573bdb9ddf
qwe5507 <apvmffkdls@gmail.com>
2025-07-11 19:20:03 +0900

    add JPA write and update example

---
+++ src/main/java/com/system/batch/killbatchsystem/jpa/write/Post.java
@@ -0,0 +1,42 @@
+package com.system.batch.killbatchsystem.jpa.write;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.BatchSize;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 게시글 엔티티 - 검열 대상
+ */
+@Entity
+@Table(name = "posts")
+@NamedQuery(
+        name = "Post.findTodayUnblockedWithReports",
+        query = "SELECT p FROM Post p JOIN FETCH p.reports r " +
+                "WHERE p.blockedAt IS NULL " +
+                "AND r.reportedAt >= :today"
+)
+@Data
+@NoArgsConstructor
+public class Post {
+    @Id
+    private Long id;
+    private String title;         // 게시물 제목
+    private String content;       // 게시물 내용
+    private String writer;        // 작성자
+
+    @OneToMany(mappedBy = "post", fetch = FetchType.EAGER) // FetchType EAGER 변경
+    @BatchSize(size = 5) // @BatchSize 적용
+    private List<Report> reports = new ArrayList<>();
+
+    private LocalDateTime blockedAt;  // 차단 일시
+}
