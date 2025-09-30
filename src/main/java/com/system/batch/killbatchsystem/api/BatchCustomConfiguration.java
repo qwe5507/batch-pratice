a994a77d00bf9684424e48c8cfca8152dfa03a82
qwe5507 <apvmffkdls@gmail.com>
2025-09-30 20:05:14 +0900

    remove duplicated bean

--- src/main/java/com/system/batch/killbatchsystem/api/BatchCustomConfiguration.java
+++ src/main/java/com/system/batch/killbatchsystem/api/BatchCustomConfiguration.java
@@ -6,12 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
 
-@Configuration
+//@Configuration
 public class BatchCustomConfiguration {
-    @Bean
-    @BatchTaskExecutor
-    public TaskExecutor taskExecutor() {
-        SimpleAsyncTaskExecutor executor = new SimpleAsyncTaskExecutor();
-        return executor;
-    }
+//    @Bean
+//    @BatchTaskExecutor
+//    public TaskExecutor taskExecutor() {
+//        SimpleAsyncTaskExecutor executor = new SimpleAsyncTaskExecutor();
+//        return executor;
+//    }
 }
