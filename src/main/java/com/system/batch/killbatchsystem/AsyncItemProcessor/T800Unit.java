88b09ec6a5518a61fcfcb970c02d2a96f2356ec3
qwe5507 <apvmffkdls@gmail.com>
2025-10-02 23:44:22 +0900

    add AsyncItemProcessor example

---
+++ src/main/java/com/system/batch/killbatchsystem/AsyncItemProcessor/T800Unit.java
@@ -0,0 +1,19 @@
+package com.system.batch.killbatchsystem.AsyncItemProcessor;
+
+public class T800Unit {
+    public T800Unit(final String s, final String s1, final String s2) {
+        throw new UnsupportedOperationException("Unsupported T800Unit");
+    }
+
+    public String getUnitId() {
+        throw new UnsupportedOperationException("Unsupported getUnitId");
+    }
+
+    public Object getIpAddress() {
+        throw new UnsupportedOperationException("Unsupported getIpAddress");
+    }
+
+    public Object getDeploymentZone() {
+        throw new UnsupportedOperationException("Unsupported getDeploymentZone");
+    }
+}
---
+++ src/main/java/com/system/batch/killbatchsystem/AsyncItemProcessor/T850Unit.java
@@ -0,0 +1,15 @@
+package com.system.batch.killbatchsystem.AsyncItemProcessor;
+
+public class T850Unit {
+    public T850Unit(final String s, final Object ipAddress, final Object deploymentZone, final String s1) {
+        throw new UnsupportedOperationException("Unsupported T850Unit");
+    }
+
+    public Object getUnitId() {
+        throw new UnsupportedOperationException("Unsupported getUnitId");
+    }
+
+    public Object getStatus() {
+        throw new UnsupportedOperationException("Unsupported getStatus");
+    }
+}
